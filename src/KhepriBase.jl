7c6a182af583f894eea12f0e141230c120c3c34b
aptmcl <antonio.menezes.leitao@gmail.com>
2020-10-04 08:38:47 +0100

    Rearranged imports.

--- src/KhepriBase.jl
+++ src/KhepriBase.jl
@@ -6,16 +6,21 @@ using Dierckx
 using Dates
 using Sockets
 
-import Base: +, -, *, /, length, show, zero, iterate, convert
-import Base: getindex, firstindex, lastindex, broadcastable
-import Base: union, fill
-import LinearAlgebra: cross, dot, norm
+import Base:
+    +, -, *, /, length,
+    show, showerror,
+    zero, iterate, convert,
+    getindex, firstindex, lastindex, broadcastable,
+    union, fill
+import LinearAlgebra:
+    cross, dot, norm
 
 include("Parameters.jl")
 include("Utils.jl")
 include("Coords.jl")
 include("Paths.jl")
 include("Geometry.jl")
+include("Backends.jl")
 include("Shapes.jl")
 include("BIM.jl")
 include("Camera.jl")
