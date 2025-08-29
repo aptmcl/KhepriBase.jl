c21fc6c56ed63ba570da187f9f3315e6b4f1efad
aptmcl <antonio.menezes.leitao@gmail.com>
2025-08-29 11:04:19 +0100

    Added thread-local parameter.

--- src/Parameters.jl
+++ src/Parameters.jl
@@ -1,4 +1,4 @@
-export with, Parameter, OptionParameter, LazyParameter
+export with, Parameter, OptionParameter, LazyParameter, ThreadLocalParameter
 
 mutable struct Parameter{T}
   value::T
@@ -22,6 +22,9 @@ with(f, p, newvalue, others...) =
     with(f, others...)
   end
 
+#=
+An OptionParameter is one which can be missing. Retrieving the value of a missing OptionParameter triggers an error.
+=#
 mutable struct OptionParameter{T}
   value::Union{Missing,T}
   OptionParameter{T}(v::T1) where {T,T1<:T} = new{T}(v)
@@ -35,6 +38,10 @@ end
     p.value::T
 (p::OptionParameter{T})(newvalue::T) where T = p.value = newvalue
 
+#=
+A LazyParameter is one which is initialized only when first requested.
+=#
+
 mutable struct LazyParameter{T,F<:Function}
   initializer::F
   value::Union{T,Nothing}
@@ -46,3 +53,20 @@ end
 (p::LazyParameter{T,F})(newvalue::T) where {T,F} = p.value = newvalue
 
 Base.reset(p::LazyParameter{T,F}) where {T,F} = p.value = nothing
+
+#=
+Khepri is becoming multithreaded. This means that Parameters are no longer a good idea,
+as different threads might interfere, where one changes, e.g., the current_cs and another
+becomes affected. Moreover, if both try to change it, read-modify-write problems might occur.
+
+To solve this problem, a ThreadLocalParameter is a variant of a Parameter that operates
+as a thread-aware dynamic variable. There is a global value, which can be consulted and/or
+updated on each thread, without interfeering with other threads.
+=#
+
+struct ThreadLocalParameter{T}
+  value::T
+end
+
+(p::ThreadLocalParameter{T})() where T = get(task_local_storage(), p, p.value)::T
+(p::ThreadLocalParameter{T})(newvalue::T) where T = task_local_storage(p, newvalue)::T
